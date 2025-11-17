bd078cfc82d6cd7afa834520ad0b6512cdc7c9c9
Jake <jboyer0000@gmail.com>
2025-11-17 11:18:42 -0500

    Cleanup whitespace

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -2,10 +2,19 @@ import sys
 import os
 import pandas as pd
 import pytest
+         
+          
+                 
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+                                   
+                   
+                   
+    
+                        
 
 import filter_and_email_report  # Import your main script here
+                                         
 
 def make_test_df():
     # Sample data simulating report with different cases
@@ -20,24 +29,42 @@ def test_filter_dispatch_zone():
     df = make_test_df()
     filtered = filter_and_email_report.apply_filters(df, dispatch="700", hide_blank_r="no", hide_driver_data="no", signed_blank="no")
     assert all(filtered["DispatchZone"] == "700")
+                                       
+                                                                          
+              
+                
+                                                 
+                    
 
 def test_hide_blank_r():
     df = make_test_df()
     filtered = filter_and_email_report.apply_filters(df, dispatch="", hide_blank_r="yes", hide_driver_data="no", signed_blank="no")
     # R should not be blank or None
     assert all(filtered["R"].astype(bool))
+                                       
+                   
+                                                  
+                                                
+                      
 
 def test_hide_driver_data():
     df = make_test_df()
     filtered = filter_and_email_report.apply_filters(df, dispatch="", hide_blank_r="no", hide_driver_data="yes", signed_blank="no")
     # Driver should be blank or None
     assert all(filtered["Driver"].isna() | (filtered["Driver"] == ""))
+                                                                 
 
 def test_show_only_blank_signedby():
     df = make_test_df()
     filtered = filter_and_email_report.apply_filters(df, dispatch="", hide_blank_r="no", hide_driver_data="no", signed_blank="yes")
     # SignedBy should be blank or None
+                                                                            
+                                         
+                                              
+                                                           
+                                     
     assert all(filtered["SignedBy"].isna() | (filtered["SignedBy"] == ""))
+             
 
 def test_combined_filters():
     df = make_test_df()
@@ -47,16 +74,57 @@ def test_combined_filters():
     assert all(filtered["R"].astype(bool))
     assert all(filtered["Driver"].isna() | (filtered["Driver"] == ""))
     assert all(filtered["SignedBy"].isna() | (filtered["SignedBy"] == ""))
+                                                   
+                                
+                                             
+                                                           
+                                     
+                                                                         
+                  
+
+           
+                                                                
+                                                
+
+                                                  
+                     
+                                                      
+                                                          
+                        
+                                                          
+         
+                                                              
+        
+                                                                                   
+
+                                                      
+
+                                                                             
+                                                                                           
     
 def test_empty_dataframe():
     df = pd.DataFrame(columns=["DispatchZone", "R", "Driver", "SignedBy"])
     filtered = filter_and_email_report.apply_filters(df, dispatch="", hide_blank_r="yes", hide_driver_data="yes", signed_blank="yes")
+    
+                             
+                                                                      
+    
+                                                                           
     assert filtered.empty
 
 def test_case_insensitivity():
     df = pd.DataFrame({"DispatchZone": ["700", "700a", "700B"]})
     filtered = filter_and_email_report.apply_filters(df, dispatch="700", hide_blank_r="no", hide_driver_data="no", signed_blank="no")
     assert len(filtered) == 3
+                                                                 
+                                      
+                                             
+             
+                
+                                                                    
+                                                        
+                                  
+                                                                    
 
 def test_no_duplicate_rows():
     df = pd.DataFrame({"OrderNumber": [1, 1, 2], "DispatchZone": ["700", "700", "800"], "R": ["a", "a", "b"], "Driver": ["x", "x", "y"], "SignedBy": ["", "", ""]})
